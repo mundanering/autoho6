3ca80d4c134407385cdeefb9e53ba3a7916e2a96
Filip <85281368+Gappy2137@users.noreply.github.com>
2023-03-05 14:46:26 +0100

    dzialajace klawisze i wejscie do gry
    guwno 1

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import subprocess
 from pathlib import Path
-import keyboard
 from time import sleep
+import pydirectinput as key
 
 FILE_DIRECTORY = str(Path(__file__).parent)
 LEPROC = FILE_DIRECTORY + r'\Locale.Emulator.2.5.0.1\LEProc.exe'
@@ -9,15 +9,15 @@ EOSD = FILE_DIRECTORY + r'\th6\eosd.exe'
 
 if __name__ == '__main__':
     subprocess.Popen([LEPROC, EOSD])
-    sleep(7)
-    keyboard.send('enter')
-    sleep(0.5)
-    keyboard.send('ctrl+down')
-    sleep(0.5)
-    # jedyne co dziala
-    keyboard.send('alt+tab')
-    sleep(0.5)
-    # tutaj tez dziala bo wyjdzie z gry
-    keyboard.send('z')
-    sleep(0.5)
-    keyboard.send('z')
+    sleep(4.7)
+    key.press('z')       # wejdz menu
+    sleep(0.01)
+    key.press('down')    # wybierz opcje extra start
+    sleep(0.01)
+    key.press('z')       # zatwierdz
+    sleep(1.9)
+    key.press('z')       # wybor postaci itd
+    sleep(0.175)
+    key.press('z')
+    sleep(0.175)
+    key.press('z')
