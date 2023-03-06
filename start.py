00e6f6dec5806f76d2b9f201d5b6003d8677397e
Filip <85281368+Gappy2137@users.noreply.github.com>
2023-03-06 19:00:15 +0100

    tera juz bedzie dzialac z vpatchem
    kuj

--- start.py
+++ start.py
@@ -1,23 +1,27 @@
 import subprocess
 from pathlib import Path
 from time import sleep
-import pydirectinput as key
+import keyboard
+
+
+def keypress(key):
+    keyboard.press(key)
+    sleep(0.05)
+    keyboard.release(key)
+
 
 FILE_DIRECTORY = str(Path(__file__).parent)
 LEPROC = FILE_DIRECTORY + r'\Locale.Emulator.2.5.0.1\LEProc.exe'
-EOSD = FILE_DIRECTORY + r'\th6\eosd.exe'
+EOSD = FILE_DIRECTORY + r'\th6\vpatch.exe'
+
 
 if __name__ == '__main__':
     subprocess.Popen([LEPROC, EOSD])
-    sleep(4.7)
-    key.press('z')       # wejdz menu
-    sleep(0.01)
-    key.press('down')    # wybierz opcje extra start
-    sleep(0.01)
-    key.press('z')       # zatwierdz
-    sleep(1.9)
-    key.press('z')       # wybor postaci itd
-    sleep(0.175)
-    key.press('z')
-    sleep(0.175)
-    key.press('z')
+    sleep(5)
+    keypress('z')       # wejdz menu
+    sleep(0.2)
+    keypress('down')    # wybierz opcje extra start
+    sleep(0.1)
+    for i in range(17):
+        keypress('z')
+        sleep(0.1)
